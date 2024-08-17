60db2632fb7a74d5b702892c3a6163fa37f0310b
parkjwi <s.lee@tuta.io>
2024-08-17 18:24:25 +0900

    Initial Stock Keep

---
+++ sk/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StockKeepConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sk'
--- urjp/settings.py
+++ urjp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'sk.apps.StockKeepConfig',
     'rest_framework.apps.RestFrameworkConfig',
     #'django.contrib.admin',
     'django.contrib.auth',
