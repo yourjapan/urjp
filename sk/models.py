125108f501b9ccf5539bc2e5055352cfa31030aa
parkjwi <s.lee@tuta.io>
2024-08-18 17:08:25 +0900

    Create Item

--- sk/models.py
+++ sk/models.py
@@ -1,3 +1,14 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.db.models import Model
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+
+class Item(Model):
+    SKU = models.CharField(unique=True, max_length=255)
+
+class ItemSerializer(ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
--- sk/views.py
+++ sk/views.py
@@ -1,5 +1,13 @@
 
 from rest_framework.routers import SimpleRouter
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+
+
+class ItemViewSet(ModelViewSet):
+    queryset = Item.objects
+    serializer_class = ItemSerializer
 
 
 router = SimpleRouter()
+router.register(r'items', ItemViewSet)
