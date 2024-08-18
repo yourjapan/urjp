b8984d641d24d078f78939ca1e0781546a3a0dab
parkjwi <s.lee@tuta.io>
2024-08-18 18:21:20 +0900

    Update Item; Create Import and Export

--- sk/models.py
+++ sk/models.py
@@ -5,10 +5,50 @@ from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 
+# ______________________________________________________________________________
+# Item
+
 class Item(Model):
     SKU = models.CharField(unique=True, max_length=255)
+    n = models.IntegerField()
 
 class ItemSerializer(ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+    imports = serializers.SerializerMethodField()
+    exports = serializers.SerializerMethodField()
+    
+    def get_imports(self, o):
+        return sum(imp.n for imp in o.imports.all())
+    
+    def get_exports(self, o):
+        return sum(exp.n for exp in o.exports.all())
+
+
+# ______________________________________________________________________________
+# Import
+
+class Import(Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='imports')
+    n = models.IntegerField()
+
+class ImportSerializer(ModelSerializer):
+    class Meta:
+        model = Import
+        fields = '__all__'
+    item = serializers.SlugRelatedField(queryset=Item.objects, slug_field='SKU')
+
+
+# ______________________________________________________________________________
+# Export
+
+class Export(Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='exports')
+    n = models.IntegerField()
+
+class ExportSerializer(ModelSerializer):
+    class Meta:
+        model = Export
+        fields = '__all__'
+    item = serializers.SlugRelatedField(queryset=Item.objects, slug_field='SKU')
--- sk/views.py
+++ sk/views.py
@@ -4,10 +4,34 @@ from rest_framework.viewsets import ModelViewSet
 from .models import *
 
 
+# ______________________________________________________________________________
+# Item
+
 class ItemViewSet(ModelViewSet):
     queryset = Item.objects
     serializer_class = ItemSerializer
 
 
+# ______________________________________________________________________________
+# Import
+
+class ImportViewSet(ModelViewSet):
+    queryset = Import.objects
+    serializer_class = ImportSerializer
+
+
+# ______________________________________________________________________________
+# Export
+
+class ExportViewSet(ModelViewSet):
+    queryset = Export.objects
+    serializer_class = ExportSerializer
+
+
+# ______________________________________________________________________________
+# Router
+
 router = SimpleRouter()
 router.register(r'items', ItemViewSet)
+router.register(r'imports', ImportViewSet)
+router.register(r'exports', ExportViewSet)
